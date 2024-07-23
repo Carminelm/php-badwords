f4078d6b490b0663a93c71be69052d22756e6036
Carminelm <carminelm54@gmail.com>
2024-07-23 17:14:03 +0200

    add form

---
+++ formget.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body> 
+   <div class="container">
+      <h1>form get</h1>
+      <form action="pagina-atterraggio.php" method="get">
+      <label for="name">Nome</label>
+    <input id="name" type="text" placeholder="Name" name="name"> <br>
+
+    <label for="lastname">Cognome</label>
+    <input id="lastname" type="text" placeholder="Lastname" name="lastname"> <br>
+
+    <button type="submit">invio</button>
+
+      </form>
+
+
+
+
+
+
+    </div>
+
+    
+</body>
+</html>
